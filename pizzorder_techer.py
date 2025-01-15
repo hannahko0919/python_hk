2b5b5fd718976d3029a78e257135d561faed76d4
hannah <aaa.com>
2025-01-15 11:24:45 +0900

    최종종

--- pizzorder_techer.py
+++ pizzorder_techer.py
@@ -16,16 +16,17 @@ def select(menu,menuname):
 
     return order
 
-def money_calc():
+def money_calc(order,menu,menuname):
     totprice=0
-for x in order.keys():
-    price=0
-    if x in menu.keys():
-         price=price+(order[x]*menu[x])   #order_pizza[x]=x의 값, ex. pizmenu['치즈피자']=3200
-    print(f'{x}({menu[x]}원) x {order[x]} = {price:,}원')
-    totprice=totprice+price
-print(f'피자 전체 가격: {totprice}')
-return totprice
+    for x in order.keys():
+        price=0
+        if x in menu.keys():
+            price=price+(order[x]*menu[x])   #order_pizza[x]=x의 값, ex. pizmenu['치즈피자']=3200
+        print(f'{x}({menu[x]}원) x {order[x]} = {price:,}원')
+        totprice=totprice+price
+
+    print(f'{menuname} 가격: {totprice:,}원')
+    return totprice
 
 
 if __name__=='__main__':
@@ -40,10 +41,11 @@ if __name__=='__main__':
     print(order_pizza)
     order_drink=select(dnkmenu,'음료수')
     print(order_drink)
-    totpiz= money_calc(order_pizza, pizmenu)
-    totdnk=money_calc(order_drink, dnkmenu)
+   
+    totpiz= money_calc(order_pizza, pizmenu,'피자')
+    totdnk=money_calc(order_drink, dnkmenu,'음료')
 
-    print(f'전체 가격: 피자 + 음료: {totpiz + totdnk}')
+    print(f'전체 가격: 피자 + 음료: {totpiz + totdnk:,}원')
 
 
 #영수증
