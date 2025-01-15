2c9a98967a93d582ccbb8bd26fcc5e0c0465fe47
hannah <aaa.com>
2025-01-15 11:31:50 +0900

    main_수정정

--- pizmaintecher.py
+++ pizmaintecher.py
@@ -1,8 +1,21 @@
-import pizzorder_techer as po
+import pizzorder_techer as p
 
-print(order_pizza())
+pizmenu={'페퍼로니 피자': 3000,
+'치즈 피자': 3200,
+'콤비네이션 피자': 3500,
+'불고기 피자':3600,
+'해산물 피자': 3800}
+dnkmenu={'콜라': 1500, '사이다': 1500,'생수': 1000}
 
-# print('='*50)
-# print('주문내역: ')
-# for name, price in pizmanu.items():
-#             print(f'{name} ({price}원) x {count}= {price*count}')
+#주문문
+order_pizza=p.select(pizmenu,'피자') 
+print(order_pizza)
+order_drink=p.select(dnkmenu,'음료수')
+print(order_drink)
+
+totpiz= p.money_calc(order_pizza, pizmenu,'피자')
+totdnk=p.money_calc(order_drink, dnkmenu,'음료')
+
+print(f'전체 가격: 피자 + 음료: {totpiz + totdnk:,}원')
+
+
